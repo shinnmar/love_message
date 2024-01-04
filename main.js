d2ac70ad28f5b7f6ba433fa51c6d3e5d5126c9f5
Shirley R <srm.shirley@gmail.com>
2024-01-04 08:21:05 -0500

    Updates on Js - hide the buttons after you have the response

--- main.js
+++ main.js
@@ -3,22 +3,19 @@ const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
+const btnGroup = document.querySelector(".btn-group");
 
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Yeiiiii! Te veo más tarde :3";
   gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
-  // this will hide the "No" btn after you click "Yes/Sip"
-  // esta línea oculta el botón "No" después de clickear "Sip"
-  noBtn.style.display = "none";
-  // this will center the button "Yes" after the "No" button dissapears
-  // esta línea centra al botón "Sip" cuando el botón "No" desaparece
-  yesBtn.style.margin = "0 auto";
+  //this will hide the buttons that are inside a div, after you have the response
+  //esto oculta a los dos botones tomando el div que los contiene, después de responder la pregunta :3
+  btnGroup.style.display = "none";
 });
 
 noBtn.addEventListener("mouseover", () => {
-  const noBtnRect = noBtn.getBoundingClientRect();
-  const maxX = window.innerWidth - noBtnRect.width;
-  const maxY = window.innerHeight - noBtnRect.height;
+  const maxX = window.innerWidth - noBtn.clientWidth;
+  const maxY = window.innerHeight - noBtn.clientHeight;
 
   const randomX = Math.floor(Math.random() * maxX);
   const randomY = Math.floor(Math.random() * maxY);
